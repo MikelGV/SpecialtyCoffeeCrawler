f547e7963a198d5f510e31f136227c544d36920e
MikelGv <mikelgaldosv@gmail.com>
2024-02-03 08:54:48 +0100

    retrieving the data from json file

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/MikelGV/SpecialtyCoffeeCrawler/crawler"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
+type Data struct {
+    Id int
+    Name string
+    Img string
+    Price string
+    Url string
+}
+
+func readData(c *gin.Context) {
+    content, err := os.ReadFile("./products.json")
+
+    if err != nil {
+        log.Fatal("Error when opening file:", err)
+    }
+
+    var payload []Data
+    err = json.Unmarshal(content, &payload)
+    if err != nil {
+        log.Fatal("Error during Unmarshal():", err)
+    }
+    c.IndentedJSON(http.StatusOK, payload)
+}
 
 func main() {
     r := gin.Default()
 
     r.GET("/crawl", func(ctx *gin.Context) {
         crawler.Crawl()
-        fmt.Println("hello world")
         ctx.JSON(200, gin.H{
             "msg": "this worked",
         })
     })
+
+    r.GET("/products", readData)
+
     r.Run("localhost:8080")
     
 }
