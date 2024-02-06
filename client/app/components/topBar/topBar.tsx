8f7c7030d53cd27eb34f9f4c26121ca2f9c8d3ea
MikelGv <mikelgaldosv@gmail.com>
2024-02-06 14:00:59 +0100

    topBar styling test

--- client/app/components/topBar/topBar.tsx
+++ client/app/components/topBar/topBar.tsx
@@ -1,8 +1,11 @@
 
 export default function TopBar() {
     return (
-        <div className="bg-green-950">
-            <h1 className="">This is the topbar</h1>
+        <div>
+            <div className="flex-col bg-yellow-950 p-0.5"></div>
+            <div className="flex-col bg-green-950 p-3 rounded-b-xl">
+                <h1 className="">This is the topbar</h1>
+            </div>
         </div>
     )
 }
