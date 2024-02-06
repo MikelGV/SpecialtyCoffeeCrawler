0bb665d8af904080915637513d8a10d2fc6387cd
MikelGv <mikelgaldosv@gmail.com>
2024-02-06 09:19:08 +0100

    general background changed from black to vanilla

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,7 @@ import TopBar from "./components/topBar/topBar";
 import Footer from "./components/footer/footer";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
         <TopBar/>
         <h1 className="text-red-700">Hello world</h1>
         <Footer/>
