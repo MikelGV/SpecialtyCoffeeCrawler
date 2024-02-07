37b2193b3f4e9cfd1604eebd50e18aa51f1a9d02
MikelGv <mikelgaldosv@gmail.com>
2024-02-07 06:09:22 +0100

    footer v1

--- client/app/components/footer/footer.tsx
+++ client/app/components/footer/footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
-        <div className="">
-            <h1>This is the footer</h1>
+        <div className="flex-col">
+            <div className="flex bg-green-950">
+                <h1>Hello there</h1>
+            </div>
+            <div className="bg-yellow-950 p-0.5"></div>
         </div>
     )
 }
