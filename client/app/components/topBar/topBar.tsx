f2949eaf8eae797e82ab9cce1fe9c1efef73e090
MikelGv <mikelgaldosv@gmail.com>
2024-02-07 03:13:47 +0100

    topbar colors added

--- client/app/components/topBar/topBar.tsx
+++ client/app/components/topBar/topBar.tsx
@@ -3,8 +3,13 @@ export default function TopBar() {
     return (
         <div>
             <div className="flex-col bg-yellow-950 p-0.5"></div>
-            <div className="flex-col bg-green-950 p-3 rounded-b-xl">
-                <h1 className="">This is the topbar</h1>
+            <div className="flex-row bg-green-950 p-3 rounded-b-xl">
+                <div id="left" className="flex-col">     
+                    <h1 className="">Home</h1>
+                </div>
+                <div id="right" className="flex-col">
+                    <h1 className="">Products</h1>
+                </div>
             </div>
         </div>
     )
