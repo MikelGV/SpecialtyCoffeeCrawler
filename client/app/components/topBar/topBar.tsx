f0e886a86f505ca8a94f6e71de405b44c6b6cbd5
MikelGv <mikelgaldosv@gmail.com>
2024-02-07 09:02:27 +0100

    links added

--- client/app/components/topBar/topBar.tsx
+++ client/app/components/topBar/topBar.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 
 export default function TopBar() {
     return (
         <div className="flex-col">
             <div className="bg-yellow-950 p-0.5"></div>
-            <div className="flex bg-green-950 p-5 rounded-b-xl font-pacifico">
-                <div id="left" className="flex-1">     
-                    <h1 className="">Home</h1>
+            <div className="flex bg-green-950 p-4 rounded-b-xl font-pacifico">
+                <div id="left" className="flex-1 pl-8">
+                    <Link className="" href="/">Home</Link>
                 </div>
-                <div id="right" className="">
-                    <h1 className="">Products</h1>
+                <div id="right" className="pr-8">
+                    <Link className="" href="/products">Products</Link>
                 </div>
             </div>
         </div>
