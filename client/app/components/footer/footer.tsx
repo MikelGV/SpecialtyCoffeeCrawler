42db67f78cc5321cfd11ac40a911c77b363d0411
MikelGv <mikelgaldosv@gmail.com>
2024-02-08 02:44:26 +0100

    added link to footer

--- client/app/components/footer/footer.tsx
+++ client/app/components/footer/footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <div className="flex-col">
             <div className="flex bg-green-950 p-10 rounded-t-xl font-pacifico justify-center">
-                <h1 className="">Made by MikelGV</h1>
+                <Link className="" href="https://mikelgv.com" target="_blank">Made by MikelGV</Link>
             </div>
             <div className="bg-yellow-950 p-0.5"></div>
         </div>
