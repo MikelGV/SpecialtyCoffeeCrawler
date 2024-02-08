d8aa039e01f552c7358ab2077aa438bcacba788d
MikelGv <mikelgaldosv@gmail.com>
2024-02-08 05:50:16 +0100

    pages folder removed and everything moved to app folder

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 import TopBar from "./components/topBar/topBar";
 import Footer from "./components/footer/footer";
-import HomeS from "./pages/home/home";
+import HomeS from "./home/home";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between">
---
+++ client/app/products/page.tsx
@@ -0,0 +1,5 @@
+export default function Products() {
+    return (
+        <div>This is product Page</div>
+    )
+}
