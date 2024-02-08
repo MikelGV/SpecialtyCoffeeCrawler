f43852ef2ca949f443a08557358b62b682e2eaf2
MikelGv <mikelgaldosv@gmail.com>
2024-02-08 08:26:36 +0100

    triangle added possibly going to change it so that it can be properly rounded

--- client/app/home/home.tsx
+++ client/app/home/home.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link"
+
 export default function HomeS() {
     return (
 
-        <div className="flex justify-center">
-            <h1 className="text-black font-pacifico text-6xl">Specialty Coffee Crawler</h1>
+        <div>
+            <div className="flex justify-center m-56">
+                <h1 className="text-black font-pacifico text-6xl">Specialty Coffee Europe</h1>
+            </div>
+            <div className="flex justify-center rounded-xl">
+                <Link href="/#companies" className="w-0 rounded-lg h-0 border-l-[50px] border-l-transparent
+                    border-t-[75px] border-t-blue-950
+                    border-r-[50px] border-r-transparent"
+                ></Link>
+            </div>
+            <div id="companies" className="flex justify-center">here goes crawled companies</div>
         </div>
     )
 }
