d5965466ea755997c4f174423b53950b5802a394
MikelGv <mikelgaldosv@gmail.com>
2024-02-09 15:49:57 +0100

    mui icons added

--- client/app/home/home.tsx
+++ client/app/home/home.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
 export default function HomeS() {
     return (
 
@@ -7,11 +7,10 @@ export default function HomeS() {
             <div className="flex justify-center m-56">
                 <h1 className="text-black font-pacifico text-6xl">Specialty Coffee Europe</h1>
             </div>
-            <div className="flex justify-center rounded-xl">
-                <Link href="/#companies" className="w-0 rounded-lg h-0 border-l-[50px] border-l-transparent
-                    border-t-[75px] border-t-blue-950
-                    border-r-[50px] border-r-transparent"
-                ></Link>
+            <div className="flex justify-center">
+                <Link href="/#companies">
+                    <ArrowDropDownRoundedIcon className="w-96 h-96 border-black bg-blue-950"/>
+                </Link>
             </div>
             <div id="companies" className="flex justify-center">here goes crawled companies</div>
         </div>
