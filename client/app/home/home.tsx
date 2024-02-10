7ca5a010fdc61fa175068d6ec76de7200dc55bd3
MikelGv <mikelgaldosv@gmail.com>
2024-02-10 09:12:43 +0100

    heroicons installed and arrow down button added Need to style it better

--- client/app/home/home.tsx
+++ client/app/home/home.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link"
-import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
+import { PlayIcon } from "@heroicons/react/16/solid"
+
 export default function HomeS() {
     return (
 
         <div>
-            <div className="flex justify-center m-56">
+            <div className="flex justify-center m-56 mb-44">
                 <h1 className="text-black font-pacifico text-6xl">Specialty Coffee Europe</h1>
             </div>
             <div className="flex justify-center">
                 <Link href="/#companies">
-                    <ArrowDropDownRoundedIcon className="w-96 h-96 border-black bg-blue-950"/>
+                    <PlayIcon className="text-blue-950 w-40 h-40 rotate-90"/>
                 </Link>
             </div>
             <div id="companies" className="flex justify-center">here goes crawled companies</div>
