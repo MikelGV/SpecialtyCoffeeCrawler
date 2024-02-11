60c9920e5b865a9fe4317a88697897c2bec27597
MikelGv <mikelgaldosv@gmail.com>
2024-02-11 17:56:04 +0100

    topbar changed

--- client/app/components/topBar/topBar.tsx
+++ client/app/components/topBar/topBar.tsx
@@ -2,16 +2,23 @@ import Link from "next/link";
 
 export default function TopBar() {
     return (
-        <div className="flex-col">
-            <div className="bg-yellow-950 p-0.5"></div>
-            <div className="flex bg-green-950 p-4 rounded-b-xl font-pacifico">
-                <div id="left" className="flex-1 pl-8">
-                    <Link className="" href="/">Home</Link>
-                </div>
-                <div id="right" className="pr-8">
-                    <Link className="" href="/products">Products</Link>
-                </div>
+        <nav className="flex font-pacifico bg-green-950 px-4 border-b md:shadow-lg items-center relative">
+            <div className="text-lg md:py-0 py-4">
+                Specialty Coffee Europe
             </div>
-        </div>
+            <ul className="md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full left-0 right-0">
+                <li>
+                    <Link href="/" className="flex md:inline-flex p-4 items-center hover:bg-gray-50">
+                        <span>Home</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/products" className="flex md:inline-flex p-4 items-center hover:bg-gray-50">
+                        <span>Products</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+            
     )
 }
