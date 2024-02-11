cced35dc03bacb697521f384a07436d72d08d115
MikelGv <mikelgaldosv@gmail.com>
2024-02-11 16:01:53 +0100

    NOMAD Coffee  added

--- client/app/home/home.tsx
+++ client/app/home/home.tsx
@@ -14,11 +14,17 @@ export default function HomeS() {
                 </Link>
             </div>
             <div id="companies" className="flex-row justify-center m-48 font-pacifico">
-                <div className="flex justify-center mb-10">
+                <div className="flex justify-center mb-20">
                     <h1 className="text-black text-4xl">Coffee Shops Crawled:</h1>
                 </div>
                 <div className="flex justify-center text-black">
-                    <div className="flex-1">Product1</div>
+                    <div id="nomad" className="flex-1">
+                        <div className="flex justify-center bg-white h-48 w-72">
+                            <div className="mt-20">
+                                <h1 className="font-grotesque font-semibold h-5 w-15">NOMAD Coffee</h1>
+                            </div>
+                        </div>
+                    </div>
                     <div className="flex-auto">Product2</div>
                     <div className="flex">Product3</div>
                 </div>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -26,6 +26,7 @@ const config: Config = {
         },
         fontFamily: {
             pacifico: ["Pacifico", "sans-serif"],
+            grotesque: ["Grotesque", "sans-serif"],
         },
     },
   },
