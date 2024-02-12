9d2ed34dbf8be2937e8e41ecdc06b042dc34424c
MikelGv <mikelgaldosv@gmail.com>
2024-02-12 14:45:07 +0100

    product v1

--- client/app/components/product/product.tsx
+++ client/app/components/product/product.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
 
 export default function Product() {
     return (
-        <div></div>
+        <div id="container" className="">
+            <div id="imgContainer" className="">
+                <Link href="">
+                     <img className="" src=""/>
+                </Link>
+            </div>
+            <div id="infoContainer" className="">
+                <h1 className=""></h1>
+                <p className=""></p>
+            </div>
+        </div>
     )
 }
