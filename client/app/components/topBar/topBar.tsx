5039edfba8fd5c9f7811d8330eeeb1242bd539d2
MikelGv <mikelgaldosv@gmail.com>
2024-02-18 22:59:59 +0100

    topbar changed for a more responsive one

--- client/app/components/topBar/topBar.tsx
+++ client/app/components/topBar/topBar.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function TopBar() {
     return (
-        <nav className="flex font-pacifico bg-green-950 px-4 border-b md:shadow-lg items-center relative">
-            <div className="text-lg md:py-0 py-4">
+        <nav className="flex font-pacifico bg-green-950 px-4 border-b md:shadow-lg items-center justify-between">
+            <h1 className="text-lg py-4">
                 Specialty Coffee Europe
-            </div>
-            <ul className="md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full left-0 right-0">
+            </h1>
+            <ul className="flex items-center space-x-6">
                 <li>
-                    <Link href="/" className="flex md:inline-flex p-4 items-center hover:bg-green-955">
+                    <Link href="/" className="p-4 hover:bg-green-955">
                         <span>Home</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/products" className="flex md:inline-flex p-4 items-center hover:bg-green-955">
+                    <Link href="/products" className="p-4 hover:bg-green-955">
                         <span>Products</span>
                     </Link>
                 </li>
