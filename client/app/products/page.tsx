f70c5b0d7af9e638e19dd298b78c127484d82844
MikelGv <mikelgaldosv@gmail.com>
2024-02-18 17:16:25 +0100

    responsive design added to product list page

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -18,37 +18,39 @@ export default async function Products() {
 
     return (
         <Layout>
-            <div className="grid grid-cols-3 gap-1 justify-evenly ml-72 mt-32 font-pacifico">
-                   {
-                Products
+            <div className="max-w-xl xl:max-w-7xl xl:mx-70 xl:my-32 md:mx-60 sm:ml-14">
+                <div className="grid grid-cols-1 gap-1 justify-evenly sm:grid-cols-2 lg:grid-cols-3 font-pacifico">
+                    {
+                        Products
 
-                    ?
-                    Products.map((p: any) => {
-                        return (
-                            <>
-                                <div id="products" className="mb-10">
-                                    <div id="imgContainer" className="">
-                                        <Link className="group" href={p.Url} target="_blank">
-                                            <img className="object-fill bg-white w-72 mb-5 rounded-xl group-hover:opacity-75" src={p.Img} />
-                                        </Link>
+                        ?
+                        Products.map((p: any) => {
+                            return (
+                                <>
+                                    <div id="products" className="mb-10">
+                                        <div id="imgContainer" className="">
+                                            <Link className="group" href={p.Url} target="_blank">
+                                                <img className="object-fill bg-white w-72 mb-5 rounded-xl group-hover:opacity-75" src={p.Img} />
+                                            </Link>
+                                        </div>
+                                        <div id="infoContainer" className="text-black w-48">
+                                            <Link className="group" href={p.Url} target="_blank">
+                                                <h1 className="group-hover:opacity-75 text-xl mb-5">{p.Name}</h1>
+                                            </Link>
+                                            <p className="mb-5 text-lg font-semibold">{p.Price}</p>
+                                        </div>
                                     </div>
-                                    <div id="infoContainer" className="text-black w-48">
-                                        <Link className="group" href={p.Url} target="_blank">
-                                            <h1 className="group-hover:opacity-75 text-xl mb-5">{p.Name}</h1>
-                                        </Link>
-                                        <p className="mb-5 text-lg font-semibold">{p.Price}</p>
-                                    </div>
-                                </div>
-                            </>
-                        )
-                    })
-                    :
+                                </>
+                            )
+                        })
+                        :
 
-                    "Getting Products ...."
+                        "Getting Products ...."
 
 
-                // THIS LOADING STATE WILL NOT BE VISIBLE BECAUSE SERVER LOADS THIS WHOLE PAGE
-            }
+                    // THIS LOADING STATE WILL NOT BE VISIBLE BECAUSE SERVER LOADS THIS WHOLE PAGE
+                }
+                </div>
             </div>
         </Layout>
     )
