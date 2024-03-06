81b071efd45b34ec7a6d29906ebdbc3c46ed3367
MikelGv <mikelgaldosv@gmail.com>
2024-03-06 18:40:23 +0100

    url changed from mikelgv.com to mikelgaldos.com

--- client/app/components/footer/footer.tsx
+++ client/app/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="flex-col">
             <div className="flex bg-green-950 p-10 font-pacifico justify-center border-t md:shadow-lg">
-                <Link className="" href="https://mikelgv.com" target="_blank">Made by MikelGV</Link>
+                <Link className="" href="https://mikelgaldos.com" target="_blank">Made by MikelGV</Link>
             </div>
         </div>
     )
